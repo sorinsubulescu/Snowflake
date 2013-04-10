28126e63cfb6936f4a319db2846a84f63a298971
sorin96 <sorinsubulescu@gmail.com>
2013-04-10 18:10:18 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snowflake::Application.config.secret_token = '8217a2ce64c183e614e4b5589a3485bf758d6a926a2309fa39b32fb71a0b4cec1d2e3cc3496229df17a5762639d5c9a6e388f5299fae5676a6a076306635a7be'
